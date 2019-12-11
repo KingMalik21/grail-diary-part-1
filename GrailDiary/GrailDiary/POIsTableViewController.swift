50d33f0480d0daee8567f4df88c261ffb02b8fec
Malik <malik21barnes@gmail.com>
2019-12-10 20:00:19 -0800

    First commit

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Malik Barnes on 12/10/19.
+//  Copyright © 2019 Malik Barnes. All rights reserved.
+//
+
+import UIKit
+class POIsTableViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
